958d1cc9dc2a0d7673ab9ad7e3040d829650a4cf
Philip Helgesen <55789033+PhilipHelgesen@users.noreply.github.com>
2021-09-20 14:26:56 +0200

    Finished task 1

---
+++ user-collections/src/main/kotlin/org/tsdes/usercollections/db/CardCopy.kt
@@ -0,0 +1,22 @@
+package org.tsdes.usercollections.db
+
+import javax.persistence.*
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+
+@Entity
+class CardCopy (
+    @get:Id @get:GeneratedValue
+    var id : Long? = null,
+
+    @get:ManyToOne
+    @get:NotNull
+    var user : User? = null,
+
+    @get:NotBlank
+    var cardId: String? = null,
+
+    @get:Min(0)
+    var numberOfCopies : Int = 0
+        )
